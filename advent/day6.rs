8d6f4a790ec934bddeb6b9aea1d82d6ef4ef88d7
Ville Heikkinen <ville.heikkinen98@hotmail.fi>
2024-12-06 17:11:27 +0200

    improved part 2 performance, clean up

--- advent/day6.rs
+++ advent/day6.rs
@@ -30,6 +30,31 @@ struct Guard {
 }
 
 impl Guard {
+    pub fn new(dir: char, guard_initial_location: (isize, isize)) -> Self {
+        match dir {
+            '^' => Guard {
+                direction: Direction::Up,
+                x: guard_initial_location.0,
+                y: guard_initial_location.1,
+            },
+            '>' => Guard {
+                x: guard_initial_location.0,
+                y: guard_initial_location.1,
+                direction: Direction::Right,
+            },
+            '<' => Guard {
+                x: guard_initial_location.0,
+                y: guard_initial_location.1,
+                direction: Direction::Left,
+            },
+            'v' => Guard {
+                x: guard_initial_location.0,
+                y: guard_initial_location.1,
+                direction: Direction::Down,
+            },
+            _ => panic!("Illegal guard symbol"),
+        }
+    }
     pub fn update_dir(&mut self) {
         self.direction = self.direction.switch_dir();
     }
@@ -43,64 +68,12 @@ impl Task for Day6 {
 
         let guard_dir = matrix[guard_location.0][guard_location.1];
         let guard_location = (guard_location.0 as isize, guard_location.1 as isize);
-        let mut guard = match guard_dir {
-            '^' => Guard {
-                direction: Direction::Up,
-                x: guard_location.0,
-                y: guard_location.1,
-            },
-            '>' => Guard {
-                x: guard_location.0,
-                y: guard_location.1,
-                direction: Direction::Right,
-            },
-            '<' => Guard {
-                x: guard_location.0,
-                y: guard_location.1,
-                direction: Direction::Left,
-            },
-            'v' => Guard {
-                x: guard_location.0,
-                y: guard_location.1,
-                direction: Direction::Down,
-            },
-            _ => panic!("Illegal guard symbol"),
-        };
-        let mut visited: HashSet<(isize, isize)> = HashSet::new();
-        visited.insert(guard_location);
-
-        matrix[guard.x as usize][guard.y as usize] = '.';
-
-        loop {
-            let (new_x, new_y) = match guard.direction {
-                Direction::Left => (guard.x, guard.y - 1),
-                Direction::Right => (guard.x, guard.y + 1),
-                Direction::Up => (guard.x - 1, guard.y),
-                Direction::Down => (guard.x + 1, guard.y),
-            };
-            if new_x >= matrix.rows as isize
-                || new_y >= matrix.columns as isize
-                || new_x < 0
-                || new_y < 0
-            {
-                break;
-            }
-
-            match matrix[new_x as usize][new_y as usize] {
-                '#' => {
-                    guard.update_dir();
-                }
-                '.' => {
-                    visited.insert((new_x, new_y));
-                    guard.x = new_x;
-                    guard.y = new_y;
-                }
-                _ => panic!("invalid char"),
-            }
-        }
+        let guard = Guard::new(guard_dir, guard_location);
+        let visited = calculate_guard_route(guard_location, &mut matrix, guard);
 
         Ok(visited.len().to_string())
     }
+
     #[allow(unused_variables)]
     fn task_part_two(&self, file_content: &str) -> Result<String, TaskError> {
         let mut matrix = Matrix::from(file_content);
@@ -108,58 +81,65 @@ impl Task for Day6 {
 
         let guard_dir = matrix[guard_location.0][guard_location.1];
         let guard_initial_location = (guard_location.0 as isize, guard_location.1 as isize);
-        let guard = match guard_dir {
-            '^' => Guard {
-                direction: Direction::Up,
-                x: guard_initial_location.0,
-                y: guard_initial_location.1,
-            },
-            '>' => Guard {
-                x: guard_initial_location.0,
-                y: guard_initial_location.1,
-                direction: Direction::Right,
-            },
-            '<' => Guard {
-                x: guard_initial_location.0,
-                y: guard_initial_location.1,
-                direction: Direction::Left,
-            },
-            'v' => Guard {
-                x: guard_initial_location.0,
-                y: guard_initial_location.1,
-                direction: Direction::Down,
-            },
-            _ => panic!("Illegal guard symbol"),
-        };
-        // let mut handles = Vec::new();
-        matrix[guard.x as usize][guard.y as usize] = '.';
-        let res: usize = matrix
-            .data()
+        let guard = Guard::new(guard_dir, guard_initial_location);
+
+        let guard2 = guard.clone();
+
+        let visited = calculate_guard_route(guard_initial_location, &mut matrix, guard);
+        let res: usize = visited
             .par_iter()
-            .enumerate()
-            .map(|(i, row)| {
-                row.iter()
-                    .enumerate()
-                    .map(|(j, element)| {
-                        if *element == '.'
-                            && !(i == guard_initial_location.0 as usize
-                                && j == guard_initial_location.1 as usize)
-                        {
-                            let mut matrix2 = matrix.clone();
-                            matrix2[i][j] = '#';
-                            check_for_loop(guard.clone(), &matrix2, guard_initial_location)
-                        } else {
-                            0
-                        }
-                    })
-                    .sum::<usize>()
+            .map(|(x, y)| {
+                let mut matrix2 = matrix.clone();
+                matrix2[*x as usize][*y as usize] = '#';
+                check_for_loop(guard2.clone(), &matrix2, guard_initial_location)
             })
             .sum();
         Ok(res.to_string())
     }
+
     fn get_day(&self) -> u32 {
         6u32
     }
+}
+
+fn calculate_guard_route(
+    guard_location: (isize, isize),
+    matrix: &mut Matrix,
+    mut guard: Guard,
+) -> HashSet<(isize, isize)> {
+    let mut visited: HashSet<(isize, isize)> = HashSet::new();
+    visited.insert(guard_location);
+
+    matrix[guard.x as usize][guard.y as usize] = '.';
+
+    loop {
+        let (new_x, new_y) = match guard.direction {
+            Direction::Left => (guard.x, guard.y - 1),
+            Direction::Right => (guard.x, guard.y + 1),
+            Direction::Up => (guard.x - 1, guard.y),
+            Direction::Down => (guard.x + 1, guard.y),
+        };
+        if new_x >= matrix.rows as isize
+            || new_y >= matrix.columns as isize
+            || new_x < 0
+            || new_y < 0
+        {
+            break;
+        }
+
+        match matrix[new_x as usize][new_y as usize] {
+            '#' => {
+                guard.update_dir();
+            }
+            '.' => {
+                visited.insert((new_x, new_y));
+                guard.x = new_x;
+                guard.y = new_y;
+            }
+            _ => panic!("invalid char"),
+        }
+    }
+    visited
 }
 
 fn check_for_loop(
