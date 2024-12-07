3916dbb22dbcbe5fbc4f031b6f3220e98a2cc6bb
Ville Heikkinen <ville.heikkinen98@hotmail.fi>
2024-12-07 16:44:11 +0200

    day 7 : Added paralellisation, moved common parts to function

--- advent/day7.rs
+++ advent/day7.rs
@@ -3,6 +3,9 @@ use std::iter::repeat_n;
 
 use crate::utils::{Task, TaskError};
 use itertools::Itertools;
+use rayon::prelude::*;
+use rayon::str::ParallelString;
+
 pub struct Day7;
 
 #[derive(Debug, Clone)]
@@ -15,43 +18,24 @@ enum Operation {
 impl Task for Day7 {
     #[allow(unused_variables)]
     fn task_part_one(&self, file_content: &str) -> Result<String, TaskError> {
-        let mut res = 0;
-        'outer: for line in file_content.lines() {
-            let (answer, inputs) = line.split_once(':').unwrap();
-
-            let answer: usize = answer.parse().unwrap();
-            let nums: Vec<usize> = inputs
-                .split_ascii_whitespace()
-                .map(|s| s.parse().unwrap())
-                .collect();
-
-            let operations = nums.len() - 1;
-            let perms: Vec<_> = repeat_n([Operation::Add, Operation::Multiply], operations)
-                .multi_cartesian_product()
-                .collect();
-
-            for oper_perm in perms {
-                let mut total = nums[0];
-                for (oper, num) in oper_perm.iter().zip(&nums[1..]) {
-                    match oper {
-                        Operation::Add => total += num,
-                        Operation::Multiply => total *= num,
-                        Operation::Concetenate => panic!("Concetenate should not be in part 1"),
-                    }
-                }
-                if total == answer {
-                    res += answer;
-                    continue 'outer;
-                }
-            }
-        }
-
+        let res = process_input(file_content, false);
         Ok(res.to_string())
     }
-    #[allow(unused_variables)]
+
     fn task_part_two(&self, file_content: &str) -> Result<String, TaskError> {
-        let mut res = 0;
-        'outer: for line in file_content.lines() {
+        let res = process_input(file_content, true);
+        Ok(res.to_string())
+    }
+
+    fn get_day(&self) -> u32 {
+        7u32
+    }
+}
+
+fn process_input(file_content: &str, is_part_2: bool) -> usize {
+    let res = file_content
+        .par_lines()
+        .map(|line| {
             let (answer, inputs) = line.split_once(':').unwrap();
 
             let answer: usize = answer.parse().unwrap();
@@ -61,12 +45,18 @@ impl Task for Day7 {
                 .collect();
 
             let operations = nums.len() - 1;
-            let perms: Vec<_> = repeat_n(
-                [Operation::Add, Operation::Multiply, Operation::Concetenate],
-                operations,
-            )
-            .multi_cartesian_product()
-            .collect();
+            let perms: Vec<_> = if is_part_2 {
+                repeat_n(
+                    [Operation::Add, Operation::Multiply, Operation::Concetenate],
+                    operations,
+                )
+                .multi_cartesian_product()
+                .collect()
+            } else {
+                repeat_n([Operation::Add, Operation::Multiply], operations)
+                    .multi_cartesian_product()
+                    .collect()
+            };
 
             for oper_perm in perms {
                 let mut total = nums[0];
@@ -78,15 +68,11 @@ impl Task for Day7 {
                     }
                 }
                 if total == answer {
-                    res += answer;
-                    continue 'outer;
+                    return answer;
                 }
             }
-        }
-
-        Ok(res.to_string())
-    }
-    fn get_day(&self) -> u32 {
-        7u32
-    }
+            0
+        })
+        .sum::<usize>();
+    res
 }
